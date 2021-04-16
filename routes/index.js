e8337c7b27e6880e102aae7399e6c7e180c1e0c1
arturo29ortiz <l161130089@gamadero.tecnm.mx>
2021-04-16 09:54:10 -0500

    Agregando nombre de compañia.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: ' Arturo Ortiz ', appName: 'WebApp' });
+  res.render('index', { title: 'Express', author: ' Arturo Ortiz ', appName: 'WebApp', company: 'Awsome Software' });
 });
 
 /* Agregando nuevba ruta*/
